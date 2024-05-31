51be6b09e7a38ff214f0f3c7efcbab88417befa8
Saul Lee <solleedata@gmail.com>
2024-06-01 07:54:37 +0900

    route to each server when click on the navigation sidebar item

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -26,7 +26,10 @@ export default function NavigationItem({
 
   return (
     <ActionTooltip label={name} side='right' align='center'>
-      <button onClick={() => {}} className='group relative flex items-center'>
+      <button
+        onClick={handleClickServer}
+        className='group relative flex items-center'
+      >
         <div
           className={cn(
             'absolute left-0 bg-primary rounded-r-full transition-all w-[4px]',
